d93c8a706fc7be3d7c7090d1eb6bac3cd6fe9d24
punitbudania <punitbudania88@gmail.com>
2020-11-24 09:52:21 +0530

    [punit]: choose b/w x and o UC 2

--- src/samplePackage/TicTacToeGame.java
+++ src/samplePackage/TicTacToeGame.java
@@ -1,5 +1,7 @@
 package samplePackage;
 
+import java.util.Scanner;
+
 public class TicTacToeGame
 {
 	public static char[] board;
@@ -8,14 +10,23 @@ public class TicTacToeGame
 	{
 		System.out.println("Welcome to Tic Tac Toe Game");
 		createBoard();
+		choice();
 	}
 	
-	public static void createBoard()
+	public static void createBoard()   //game board created
 	{
 		board = new char[10];
 		for(int i=1; i<10; i++)
 		{
 			board[i] = ' ';
 		}
+	}
+	
+	public static void choice()     // player can choose between x and o.
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Choose x or o");
+		String c = sc.next();
+		System.out.println("you choose: " + c);
 	}
 }
